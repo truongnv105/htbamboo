7e2a8447cd2ef907d32ebd013c2f994860c58e50
TruongNV <truongnv105>
2020-12-14 21:39:17 +0700

    Add authenticate admin

---
+++ deploy/deploy_dev.sh
@@ -0,0 +1,25 @@
+############# First init project
+# composer require laravel/ui
+# composer require laravel/ui
+### Scaffolding bootstra, js
+# php artisan ui bootstrap
+### first init
+
+
+####### Install labrary js, bootstrap
+echo "==============================";
+# npm install;
+npm run dev;
+echo "Run compiler js and css";
+echo "==============================";
+
+####### Clear cache, view, configuration
+echo "==============================";
+
+php artisan view:clear;
+php artisan cache:clear;
+php artisan config:clear;
+echo "Clear all project";
+echo "==============================";
+
+
---
+++ resources/lang/en/resources.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "AD_website_name" => "Furniture Rattan",
+    "AD_products" => "Products",
+    "AD_categories" => "Categories"
+];
